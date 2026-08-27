e06162733c5472f5526ff5964b65e2704c3248f4
yasmin <yasmiin.oli17@gmail.com>
2026-08-27 14:56:33 -0300

    Edição de página de Eventos

--- routes/events.js
+++ routes/events.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('events/index', {title: 'Eventos do NUARTE', subtitle:'Gerencie os eventos realizados pelo NUARTE.'});
 });
 
 module.exports = router;
