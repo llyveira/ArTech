070f786f35eb988bf23e4325627ae15e759f6f0f
Isabelly <v.guerlain@escolar.ifrn.edu.br>
2026-09-03 14:25:27 -0300

    Costumização e poup up da tela de eventos

---
+++ public/javascripts/events.js
@@ -0,0 +1,8 @@
+
+function abrirModalNovoEvento() {
+  document.getElementById('modalNovoEvento').classList.remove('hidden');
+}
+
+function fecharModalNovoEvento() {
+  document.getElementById('modalNovoEvento').classList.add('hidden');
+}
